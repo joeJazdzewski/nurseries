8dcfb091e68ae46611b1200106715436f62723ce
joe jazdzewski <joejazdzewski@gmail.com>
2019-11-28 10:15:30 -0800

    INursery is Disposible

--- Nurseries/Nursery.cs
+++ Nurseries/Nursery.cs
@@ -5,6 +5,21 @@ namespace Nurseries
 {
     public class Nursery : INursery
     {
-        
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+
+        public Task StartSoon() 
+        {
+            return Task.FromException(new NotImplementedException());
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            
+        }
+
+
     }
 }
